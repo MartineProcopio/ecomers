0f56de78d40b541af34d5342372d4525f0735cb7
Martin <martineprocopio2020@gmail.com>
2023-01-24 19:40:59 -0300

    Agregamos Bata Base

--- src/reuters/web.routes.js
+++ src/reuters/web.routes.js
@@ -1,9 +1,19 @@
 const express = require('express');
-
+const models = require('./../database/models')
 const webRouter = express.Router();//instancia de routeador
 
 webRouter.get('/',(req,res)=>{
     res.render('index')
+})
+
+webRouter.post('/products',async(req,res)=>{
+    const product =  await models.Product.create({name:'Remera',price:100.50})
+    res.send(product)
+})
+
+webRouter.get('/products',async(req,res)=>{
+    const products =  await models.Product.findAll()
+    res.send(products)
 })
 
 module.exports = webRouter
